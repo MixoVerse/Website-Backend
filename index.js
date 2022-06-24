f2e6cac945cf68833cabab88523b033d20972e29
Xenorio <hi@xenorio.xyz>
2022-06-25 00:31:14 +0200

    add cors

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const express = require('express')
 const fetch = require('cross-fetch')
 const ms = require('ms')
 const { createMollieClient } = require('@mollie/api-client');
+const cors = require('cors')
 
 var app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.use((req, res, next) => {
     res.set('x-powered-by', "MixoVerse Backend")
